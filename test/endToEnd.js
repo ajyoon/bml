e7b63373e2e67501618237a62b213a2bc512bdb3
Andrew Yoon <ayoon@toasttab.com>
2018-02-25 19:58:28 -0500

    Add an end to end test for bml documents without preludes

---
+++ test/endToEnd.js
@@ -0,0 +1,24 @@
+let assert = require('assert');
+let fs = require('fs');
+
+let bml = require('bml');
+
+describe('bml', function() {
+  it('doesn\'t explode when trying to process a sample document', function() {
+    let testString = '' + fs.readFileSync(require.resolve('../sample.bml'));
+    bml(testString);
+    // If we make it here without an exception, we win
+  });
+
+  it('can process a document without a prelude', function() {
+    let testString = "hello {{'beautiful' 60, 'wonderful'}} world!";
+    let result = bml(testString);
+    let possibleOutcomes = [
+      'hello beautiful world!',
+      'hello wonderful world!',
+    ];
+    if (possibleOutcomes.indexOf(result) === -1) {
+      assert.fail(`Unexpected output: ${result}`);
+    }
+  });
+});
