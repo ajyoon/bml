68adc8e23d02fe02a2691aa1ac36928ff66df4cf
Andrew Yoon <andrewyoon2@gmail.com>
2018-03-03 03:01:53 -0500

    Use combined modes for bml with/without prelude

--- src/highlight/highlightTesting.js
+++ src/highlight/highlightTesting.js
@@ -2,22 +2,6 @@ let fs = require('fs');
 
 let hljs = require('highlight.js');
 
-let SPLIT_SRC_RE = /(^(.*?)begin (use|using) (\w*?)\n)(.*)/s;
-
-/**
- * Split a bml source into its prelude and body
- *
- * @returns {String[]} The prelude and body of the bml source.
- *     If not prelude is present, return an array of just the body.
- */
-function splitBmlCode(source) {
-  let match = source.match(SPLIT_SRC_RE);
-  if (match === null) {
-    return source;
-  }
-  return [match[1], match[5]];
-}
-
 function multiLineApostropheStringMode(hljs) {
   return {
     className: 'string',
@@ -34,51 +18,14 @@ function multiLineQuoteStringMode(hljs) {
   };
 }
 
-function bmlEvalMode(hljs) {
-  return {
-    beginKeywords: 'eval evaluate',
-    starts: {
-      end: '^}$',
-      returnEnd: true,
-      subLanguage: ['javascript'],
-    },
-  };
-}
-
-/**
- * hljs mode for a bml `mode` block
- */
-function bmlModeMode(hljs) {
-  return {
-    keywords: 'mode as call',
-    begin: '^mode',
-    end: '^}$',
-    contains: [
-      multiLineApostropheStringMode(hljs),
-      multiLineQuoteStringMode(hljs),
-      hljs.NUMBER_MODE,
-      hljs.C_LINE_COMMENT_MODE,
-    ],
-  };
-}
-
-function bmlPreludeLanguage(hljs) {
-  return {
-    className: 'bml-prelude',
-    keywords: 'eval evaluate mode begin use using as',
-    contains: [
-      hljs.C_LINE_COMMENT_MODE,
-      bmlEvalMode(hljs),
-      bmlModeMode(hljs),
-    ],
-  };
-}
 
 function bmlLiteralMode(hljs) {
   return {
     className: 'string',
     begin: '\\[\\[',
     end: '\\]\\]',
+    excludeBegin: true,
+    excludeEnd: true,
   };
 }
 
@@ -88,6 +35,7 @@ function bmlTransformMode(hljs) {
     keywords: 'as call',
     begin: '{{',
     end: '}}',
+    relevance: 10,
     contains: [
       multiLineApostropheStringMode(hljs),
       multiLineQuoteStringMode(hljs),
@@ -109,23 +57,66 @@ function bmlBodyLanguage(hljs) {
   };
 }
 
-hljs.registerLanguage('bml_prelude', bmlPreludeLanguage);
-hljs.registerLanguage('bml_body', bmlBodyLanguage);
-
-
-function highlightBml(source) {
-  splitCode = splitBmlCode(source);
-  if (typeof splitCode === 'string') {
-    return hljs.highlight('bml_body', source).value;
-  }
-  let highlightedPrelude = hljs.highlight('bml_prelude', splitCode[0]);
-  let highlightedBody = hljs.highlight('bml_body', splitCode[1]);
-  return highlightedPrelude.value + '\n' + highlightedBody.value;
+function bmlEvalMode(hljs) {
+  return {
+    beginKeywords: 'eval evaluate',
+    starts: {
+      end: '^}$',
+      returnEnd: true,
+      subLanguage: ['javascript'],
+    },
+  };
 }
 
+function bmlModeOpeningMode(hljs) {
+  return {
+    keywords: 'mode',
+    begin: '^mode',
+    end: '{',
+    contains: [
+      hljs.inherit(hljs.TITLE_MODE, {begin: hljs.IDENT_RE}),
+    ],
+  };
+}
+
+function bmlBeginMode(hljs) {
+  return {
+    keywords: 'begin use using',
+    begin: '^begin (use|using)',
+    end: '\w+',
+    endsParent: true,
+    relevance: 100,
+  };
+}
+
+function bmlLanguage(hljs) {
+  return {
+    className: 'bml-prelude',
+    keywords: 'eval evaluate mode begin use using as call r',
+    end: '^begin (use|using) (\w+)$',
+    contains: [
+      hljs.C_LINE_COMMENT_MODE,
+      bmlEvalMode(hljs),
+      multiLineApostropheStringMode(hljs),
+      multiLineQuoteStringMode(hljs),
+      hljs.NUMBER_MODE,
+      hljs.C_LINE_COMMENT_MODE,
+      bmlModeOpeningMode(hljs),
+      bmlBeginMode(hljs),
+    ],
+    starts: bmlBodyLanguage(hljs),
+  };
+}
+
+hljs.registerLanguage('bml', bmlLanguage);
+hljs.registerLanguage('bml_body_only', bmlBodyLanguage);
+
+function highlightBml(source) {
+  return hljs.highlightAuto(source, ['bml', 'bml_body_only']).value;
+}
 
 let bmlSrc = '' + fs.readFileSync('./highlight_sample_2.bml');
-let css = '' + fs.readFileSync('./node_modules/highlight.js/styles/railscasts.css');
+let css = '' + fs.readFileSync('./node_modules/highlight.js/styles/darcula.css');
 
 
 let highlighted = highlightBml(bmlSrc);
@@ -144,6 +135,6 @@ ${highlighted}
 </pre>
 </body>
 </html>
-`
+`;
 
 fs.writeFileSync('out.html', output);
