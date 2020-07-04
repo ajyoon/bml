b8b34e889697136f88a5a7e2d6b9fa872126069d
Andrew Yoon <andrew@nothing-to-say.org>
2020-07-03 23:12:13 -0400

    remove preview testing folder

--- preview_testing/main.js
+++
@@ -1,7 +0,0 @@
-$('#bml-input').keyup(function (event) {
-  var highlightedHtml = bmlHighlighting.highlightBml($(this).val());
-  $('#bml-highlighted').html(highlightedHtml);
-
-  var renderedBml = bml($(this).val());
-  $('#bml-preview').html(renderedBml);
-});
