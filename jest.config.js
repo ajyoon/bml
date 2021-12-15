2b887eedad2ffb4bcc2025f594d9f611c92d6751
Andrew Yoon <andrew@nothing-to-say.org>
2021-12-15 11:53:20 -0500

    Add release tests which run against dist artifacts
    This will help guarantee stability amidst all this new tooling and
    build complication

--- jest.config.js
+++ jest.config.js
@@ -151,9 +151,9 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "releaseTest.js"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
